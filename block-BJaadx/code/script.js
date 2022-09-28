f604f3e91a28696b9d9d9c79192e26202fc12e9a
rahul raushan <raushanrahul23@gmail.com>
2022-09-28 06:08:43 +0530

    Selecting Elements

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,30 +1,36 @@
 // 1. Change the title of the page to `Hello AltCampus!`
+document.title = " Hello altcampus";
 
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
+document.body.children[0].h1 = "Learning DOM"
 
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
+document.body.children[5].children[0].innerText = "all about Document";
 //    - Select the input element with name `email`
-
+document.body.children[6].children[2];
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-
+let li = document.querySelector(".topics").children.length;
+console.log(li)
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
+let emailInput  = document.querySelector("fieldset input");
 
 // 5. Select the ul element using class selector and store in `topics`
-
+document.querySelector(".topics");
 // 6. Select the first label element and store in `label`
-
+let label = document.querySelector("fieldset label");
 // 7. Select the input of type `checkbox` with the `id` selector and store in `inputCheckbox`
-
+let inputCheckbox  = document.querySelector("#remember");
 // 8. Select the input of type password using Attribute selectors. (eg: input[type="text"]) and store in `password`
-
+password = document.querySelector( 'input[type ="password"]');
 // 9. Select the input using the placeholder attribute selector with value `password` and store in `attrPassword`
+attrPassword = document.querySelector(`input[type ="password"]`);
 
 // 10. Select all the `li` element and store in `allTopics`
-
+allTopics = document.querySelector(".topics").children;
 // 11. Select all the input element of any type and store in `allInput`
-
+allInput = document.querySelectorAll("fieldset input")
 // 12. Use forEach to console the `innerText` property of all the li element in `allTopics` variable.
 
 // 13. Select all the elements with class `list` and store in variable `listOfSelectedTopics`
