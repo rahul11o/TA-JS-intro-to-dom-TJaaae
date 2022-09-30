2042e5c0896ee127d9f423f4e336096cf3fe5805
rahul raushan <raushanrahul23@gmail.com>
2022-09-30 08:40:44 +0530

    Assignment-Manipulating-Elements-2

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -5,7 +5,7 @@ default value to be "text" and return the input element inside label. (create it
 
 */
 
-// Your code goes here
+//
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
